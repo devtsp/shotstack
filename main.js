b8a197ae30e62cb75372456b17f156aac58c9f34
Tomas <paeztms@gmail.com>
2022-09-11 02:49:19 -0300

    add interface to submit JSON format string and retrieve video

--- main.js
+++ main.js
@@ -1,33 +1,67 @@
 import { renderAsset, checkRenderStatus, getAsset } from './api.js';
 
-const $editBody = document.getElementById('edit-body');
-const $edit = document.getElementById('edit');
-const $editResult = document.getElementById('edit-result');
+const $submitBtn = document.getElementById('submit');
+const $spinnerSubmit = document.getElementById('render-spinner');
+const $inputBody = document.getElementById('body');
 
-const $statusRenderId = document.getElementById('render-id-status');
+const $retrieveBtn = document.getElementById('retrieve');
+const $spinnerResult = document.getElementById('result-spinner');
 const $status = document.getElementById('status');
-const $statusResult = document.getElementById('status-result');
+const $result = document.getElementById('result');
 
-const $getRenderId = document.getElementById('render-id-get');
-const $get = document.getElementById('get');
-const $getResult = document.getElementById('get-result');
+let renderId;
 
-$edit.onclick = async () => {
-	console.log(
-		JSON.stringify($editBody.value.replace(/(\r\n|\n|\r|\s)/gm, ''), null, 2)
-	);
-	const renderResult = await renderAsset(
-		JSON.stringify($editBody.value.replace(/(\r\n|\n|\r|\s)/gm, ''))
-	);
-	$editResult.innerText = JSON.stringify(renderResult, null, 2);
+$submitBtn.onclick = async () => {
+  $spinnerSubmit.style.display = 'block';
+  $inputBody.style.opacity = '0.5';
+  $submitBtn.style.opacity = '0.5';
+  $submitBtn.innerText = 'SUBMITTING...';
+  try {
+    const renderResult = await renderAsset(
+      $inputBody.value.replace(/(\r\n|\n|\r)/gm, '')
+    );
+    console.log(renderResult);
+    renderId = renderResult.response.id;
+    alert('Render queued!! you can get the result when rendering done');
+  } catch (err) {
+    console.log(err);
+    alert(err);
+  } finally {
+    $spinnerSubmit.style.display = 'none';
+    $inputBody.style.opacity = '1';
+    $submitBtn.style.opacity = '1';
+    $submitBtn.innerText = 'SUBMIT';
+  }
 };
 
-$status.onclick = async () => {
-	const renderStatus = await checkRenderStatus($statusRenderId.value);
-	$statusResult.innerText = JSON.stringify(renderStatus, null, 2);
-};
-
-$get.onclick = async () => {
-	const servedAsset = await getAsset($getRenderId.value);
-	$getResult.innerText = JSON.stringify($getRenderId, null, 2);
+$retrieveBtn.onclick = async () => {
+  $spinnerResult.style.display = 'block';
+  $status.innerText = '';
+  $retrieveBtn.style.opacity = '0.5';
+  $result.style.opacity = '0.5';
+  $retrieveBtn.innerText = 'CHECKING STATUS...';
+  console.log('About to fetch id: ', renderId);
+  try {
+    const status = await checkRenderStatus(renderId);
+    console.log(status.response);
+    if (status.response.status === 'rendering') {
+      $status.innerText = 'Still rendering. Try again later';
+    } else if (status.response.status === 'done') {
+      const $link = document.createElement('a');
+      $link.innerText = status.response.url;
+      $link.href = status.response.url;
+      $status.appendChild($link);
+      const $sourceTag = document.createElement('source');
+      $sourceTag.src = status.response.url;
+      $result.querySelector('video').innerHTML = '';
+      $result.querySelector('video').appendChild($sourceTag);
+    }
+  } catch (err) {
+    console.log(err);
+  } finally {
+    $spinnerResult.style.display = 'none';
+    $retrieveBtn.style.opacity = '1';
+    $result.style.opacity = '1';
+    $retrieveBtn.innerText = 'RETRIEVE RENDER';
+  }
 };
